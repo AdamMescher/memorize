9143de13c5fbaf448b3eb3a60158a3dedbbaa2a5
Adam Mescher <admescher@gmail.com>
2021-11-17 17:52:45 -0700

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Adam Mescher on 11/17/21.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
